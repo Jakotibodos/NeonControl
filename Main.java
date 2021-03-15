1dde4bdba4a6e41f9c099dca134b304131877f12
Jakotibodos <70334746+Jakotibodos@users.noreply.github.com>
2021-03-15 16:43:05 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,11 +11,18 @@ package neoncontrol;
  */
 public class NeonControl {
 
-    /**
-     * @param args the command line arguments
-     */
+   public void start(Stage stage) throws Exception{
+    
+        // Create a scene and place it in the stage
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("NeonControl"); // Set the stage title
+        stage.setScene(scene); // Place the scene in the stage
+        stage.show(); // Display the stage
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
