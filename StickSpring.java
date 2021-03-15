13b2f13c89c92b96208fefe89ec994c2c302081d
Jakotibodos <70334746+Jakotibodos@users.noreply.github.com>
2021-03-15 16:34:42 -0400

    Update StickSpring.java

--- StickSpring.java
+++ StickSpring.java
@@ -16,40 +16,43 @@ import javafx.scene.shape.Rectangle;
  * @author addav
  */
 public class StickSpring extends ImageView{
-    private double YPos;
+    private double yPos;
     private double xPos;
     private double angle = 0;
     
     private Vector velVector;
     private double mass;
     
-    private Image image;
-    
     private Rectangle spring1HB;
     private Rectangle spring2HB;
     private Rectangle stickHB;
 
     public StickSpring() {
+        this.setImage(new Image("Graphics/spring 1.png"));
+        setPos(200,200);
     }
     
     public StickSpring(Vector velVector){
         this.velVector = velVector;
+        this.setImage(new Image("Graphics/spring 1.png"));
     }
 
     public double getYPos() {
-        return YPos;
+        return yPos;
     }
 
-    public void setYPos(double YPos) {
-        this.YPos = YPos;
+    public void setYPos(double yPos) {
+        this.yPos = yPos;
+        this.setY(yPos);
     }
 
-    public double getxPos() {
+    public double getXPos() {
         return xPos;
     }
 
-    public void setxPos(double xPos) {
+    public void setXPos(double xPos) {
         this.xPos = xPos;
+        this.setX(xPos);
     }
 
     public double getAngle() {
@@ -58,6 +61,7 @@ public class StickSpring extends ImageView{
 
     public void setAngle(double angle) {
         this.angle = angle;
+        this.setRotate(angle);
     }
 
     public Vector getVelocityVec(){
@@ -75,7 +79,8 @@ public class StickSpring extends ImageView{
     public void setMass(double mass) {
         this.mass = mass;
     }
-
+    
+    //this will need fixing
     public void rotate(){
         double currentAngle = this.getAngle();
     }
@@ -91,7 +96,8 @@ public class StickSpring extends ImageView{
     }
     
     public void setPos(double newXPos, double newYPos){
-        
+        this.setXPos(newXPos);
+        this.setYPos(newYPos);
     }  
     
     public Rectangle getHB1() {
